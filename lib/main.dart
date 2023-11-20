b2f1eea9464beaae9ace276d48fcdceb05a5f2c3
Ahmed Mohamed Yousef <a41155098@gmail.com>
2023-11-20 09:20:55 +0200

    refresh branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_complete_project/core/routing/app_router.dart';
 import 'package:flutter_complete_project/doc_app.dart';
 
 void main() {
-  print("FAke push");
   runApp(
     DocApp(
       appRouter: AppRouter(),
