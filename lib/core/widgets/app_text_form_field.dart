1378da299b08bc530037c3ed19670fd277c01b25
Ahmed Mohamed Yousef <a41155098@gmail.com>
2023-12-08 18:28:18 +0200

    * added spacing file * added some colors & styles * added app text button & app text form field * added login screen

---
+++ lib/core/widgets/app_text_form_field.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/theming/colors.dart';
+import 'package:flutter_complete_project/core/theming/styles.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextFormField extends StatelessWidget {
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final bool? isObscureText;
+  final Widget? suffixIcon;
+  final Color backgroundColor;
+
+  const AppTextFormField({
+    super.key,
+    this.contentPadding,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.inputTextStyle,
+    this.hintStyle,
+    required this.hintText,
+    this.isObscureText,
+    this.suffixIcon,
+    this.backgroundColor = ColorsManager.moreLightGray,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        isDense: true,
+        contentPadding: contentPadding ??
+            EdgeInsets.symmetric(
+              vertical: 20.w,
+              horizontal: 18.h,
+            ),
+        focusedBorder: focusedBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: ColorsManager.mainBlue,
+                width: 1.2,
+              ),
+              borderRadius: BorderRadius.circular(16),
+            ),
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: ColorsManager.lighterGray,
+                width: 1.2,
+              ),
+              borderRadius: BorderRadius.circular(16),
+            ),
+        hintStyle: hintStyle ?? TextStyles.font14GrayRegular,
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: backgroundColor,
+      ),
+      obscureText: isObscureText ?? false,
+      style: inputTextStyle ?? TextStyles.font14DarkBlueMedium,
+    );
+  }
+}
--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,16 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/helper/spacing.dart';
+import 'package:flutter_complete_project/core/theming/styles.dart';
+import 'package:flutter_complete_project/core/widgets/app_text_button.dart';
+import 'package:flutter_complete_project/core/widgets/app_text_form_field.dart';
+import 'package:flutter_complete_project/features/login/ui/widgets/already_have_account_text.dart';
+import 'package:flutter_complete_project/features/login/ui/widgets/terms_and_conditions_text.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObsecure = true;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login screen'),
-      ),
-      body: const Center(
-        child: Text('Login screen'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcom Back",
+                  style: TextStyles.font24BlueBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  "We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
+                  style: TextStyles.font14GrayRegular,
+                ),
+                verticalSpace(36),
+                Form(
+                  child: Column(
+                    children: [
+                      const AppTextFormField(hintText: "Email"),
+                      verticalSpace(18),
+                      AppTextFormField(
+                        hintText: "Password",
+                        isObscureText: isObsecure,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isObsecure = !isObsecure;
+                            });
+                          },
+                          child: Icon(
+                            isObsecure
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                      verticalSpace(24),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyles.font13BlueRegular,
+                        ),
+                      ),
+                      verticalSpace(40),
+                      AppTextButton(
+                        buttonText: "LOGIN",
+                        textStyle: TextStyles.font16WhiteSemiBold,
+                        onPressed: () {},
+                      ),
+                      verticalSpace(16),
+                      const TermsAndConditionsText(),
+                      verticalSpace(60),
+                      const AlreadyHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
