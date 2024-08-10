6cd55144e523989ae53ea1b8a7288fc9df882a53
Ahmed Mohamed Yousef <ahmed.moyousef98@gmail.com>
2024-08-10 10:54:27 +0300

    -login ui

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,16 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_complete_project/core/helpers/spacing.dart';
+import 'package:flutter_complete_project/core/theming/styles.dart';
+import 'package:flutter_complete_project/core/widgets/app_text_button.dart';
+import 'package:flutter_complete_project/core/widgets/app_text_form_field.dart';
+import 'package:flutter_complete_project/features/login/ui/widgets/already_have_account_text.dart';
+import 'package:flutter_complete_project/features/login/ui/widgets/terms_and_conditions_text.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObsecure = true;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('LoginScreen'),
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome Back',
+                style: TextStyles.font24BlueBold,
+              ),
+              verticalSpacing(8),
+              Text(
+                'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                style: TextStyles.font14GrayRegular,
+              ),
+              verticalSpacing(36),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    const AppTextFormField(hintText: "Email"),
+                    verticalSpacing(18),
+                    AppTextFormField(
+                      hintText: "Password",
+                      suffixIcon: GestureDetector(
+                        onTap: () => setState(() {
+                          isObsecure = !isObsecure;
+                        }),
+                        child: Icon(
+                          isObsecure ? Icons.visibility_off : Icons.visibility,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyles.font13BlueRegular,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    verticalSpacing(40),
+                    AppTextButton(
+                        buttonText: 'Login',
+                        textStyle: TextStyles.font16WhiteSemiBold,
+                        onPressed: () {}),
+                    verticalSpacing(16),
+                    const TermsAndConditionsText(),
+                    verticalSpacing(60),
+                    const AlreadyHaveAccountText()
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-        body: Center(
-          child: Text('LoginScreen is working'),
-        ));
+      ),
+    ));
   }
 }
