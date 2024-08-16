86ba888e6bff7af3c23779336b4922157f8975e9
Ahmed Mohamed Yousef <ahmed.moyousef98@gmail.com>
2024-08-16 19:00:06 +0300

    - add .vscode folder - configure android build gradle - configure android manifest file - delete main.dart file - create main_developement.dart and main_production.dart

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_project/core/di/dependancy_injection.dart';
 import 'package:flutter_complete_project/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routing/app_router.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
-  runApp(DocApp(
-    appRouter: AppRouter(),
-  ));
+  // to fix .sp font bug in screenUtil, in release mode
+  await ScreenUtil.ensureScreenSize();
+  runApp(DocApp(appRouter: AppRouter()));
 }
